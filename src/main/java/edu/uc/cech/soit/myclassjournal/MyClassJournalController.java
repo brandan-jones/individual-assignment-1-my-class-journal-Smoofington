59f416f124982924a2a2739760d9ea7ded4b2c53
Smoofington <caudill.matthew0127@gmail.com>
2022-10-09 10:50:54 -0500

    -added DAO layer class and interface -added JournalServiceStub -added a get and a post endpoint in MyClassJournalController. -changed the title of the start.html page -added some tags and text to start.html -added @Data annotation to DTO

--- src/main/java/edu/uc/cech/soit/myclassjournal/MyClassJournalController.java
+++ src/main/java/edu/uc/cech/soit/myclassjournal/MyClassJournalController.java
@@ -1,10 +1,22 @@
 package edu.uc.cech.soit.myclassjournal;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import edu.uc.cech.soit.myclassjournal.dto.JournalEntry;
+import edu.uc.cech.soit.myclassjournal.service.IJournalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@Controller
 public class MyClassJournalController {
+
+    @Autowired
+    IJournalService journalService;
+
     /**
      * Handle the / endpoint
+     *
      * @return
      */
     @RequestMapping("/")
@@ -12,5 +24,36 @@ public class MyClassJournalController {
         return "start";
     }
 
+    /**
+     * Fetches all Journal Entries
+     *
+     * @return a collection of Journal Entries
+     */
+    @GetMapping("/journal-entries")
+    @ResponseBody
+    public List<JournalEntry> fetchAllJournalEntries() {
+        return journalService.fetchAll();
+    }
 
+    /**
+     * Create a new journal entry object, given the data provided
+     *
+     * returns on of the following status codes:
+     * 201: successfully created a new journal entry.
+     * 409: unable to create a journal entry, because it already exists.
+     *
+     * @param journalEntry a JSON representation of a journal entry object.
+     * @return the newly created journal entry object.
+     */
+    @PostMapping(value = "/journal-entries", consumes = "application/json", produces = "application/json")
+    @ResponseBody
+    public JournalEntry createJournalEntry(@RequestBody JournalEntry journalEntry) {
+        JournalEntry newJournalEntry = null;
+        try {
+            newJournalEntry = journalService.save(journalEntry);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return newJournalEntry;
+    }
 }
